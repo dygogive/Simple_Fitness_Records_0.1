edd61b3197fc320bad2291014098b19980cf24f4
Roman <38737614+dygogive@users.noreply.github.com>
2023-04-06 08:00:49 +0300

    0604.2023

--- app/src/main/java/com/example/simplefitnessrecords01/UI_activities/MusclesGroupsActivity.java
+++ app/src/main/java/com/example/simplefitnessrecords01/UI_activities/MusclesGroupsActivity.java
@@ -3,17 +3,21 @@ package com.example.simplefitnessrecords01.UI_activities;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.AbsListView;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ListView;
+import android.widget.SimpleExpandableListAdapter;
 import android.widget.Toast;
 
 import com.example.simplefitnessrecords01.R;
 import com.example.simplefitnessrecords01.databinding.ActivityMusclesGroupsBinding;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -33,9 +37,13 @@ public class MusclesGroupsActivity extends AppCompatActivity {
         actionBar.setSubtitle("Select group");
         actionBar.setDisplayHomeAsUpEnabled(true);
 
-        //initExpandableListView();
 
-        initListView();
+        //check if there is ExpandableListview in the activity, check if there is Listview in the activity
+        if( binding.lvGroups instanceof ExpandableListView) {
+            initExpandableListView();
+        } else if ( binding.lvGroups instanceof ListView) {
+            initListView();
+        }
     }
 
 
@@ -54,14 +62,43 @@ public class MusclesGroupsActivity extends AppCompatActivity {
         });
     }
 
+
     private void initExpandableListView() {
-        //Array from resource
-        String[] groups = getResources().getStringArray(R.array.muscles_groups);
-//        String[] groups = getResources().getStringArray(R.array.muscles_groups);
-//        String[] groups = getResources().getStringArray(R.array.muscles_groups);
-//        String[] groups = getResources().getStringArray(R.array.muscles_groups);
-//        String[] groups = getResources().getStringArray(R.array.muscles_groups);
-//        String[] groups = getResources().getStringArray(R.array.muscles_groups);
+
+
+        //List og groups from resource
+        List<String> groups = Arrays.asList(getResources().getStringArray(R.array.muscles_groups));
+        //List og groups with key
+        List<Map<String,String>> groupsKey = createGroupData(groups, "keyGroups");
+
+        //List of childs without key
+        List<String[]> childs = new LinkedList<>();
+        childs.add(getResources().getStringArray(R.array.lower_body));
+        childs.add(getResources().getStringArray(R.array.back));
+        childs.add(getResources().getStringArray(R.array.chest));
+        childs.add(getResources().getStringArray(R.array.shoulders));
+        childs.add(getResources().getStringArray(R.array.arms));
+        childs.add(getResources().getStringArray(R.array.abdominals));
+        //list with key
+        List<List<Map<String, String>>> childsKey = createChildData(childs , "keyChilds");
+
+        //
+        String[] groupFrom = new String[] {"keyGroups"  };
+        int[] groupTo      = new int[] {R.id.tv_groups};
+        //
+        String[] childFrom = new String[] {"keyChilds"  };
+        int[] childTo      = new int[] {R.id.tv_muscles};
+
+        ExpandableListView expandableListView = (ExpandableListView) binding.lvGroups;
+        SimpleExpandableListAdapter adapter = new SimpleExpandableListAdapter(
+                (Context) MusclesGroupsActivity.this,
+                groupsKey, R.layout.text_list_groups_body, groupFrom, groupTo,
+                childsKey, R.layout.text_list_groups_muscles , childFrom, childTo
+        );
+        expandableListView.setAdapter(adapter);
+
+
+
 
 
 
@@ -70,14 +107,46 @@ public class MusclesGroupsActivity extends AppCompatActivity {
     }
 
 
-    private Map<String,String> getListWithKey(List<String> list, String key) {
-        Map<String,String> map = new HashMap<>();
+    private List<List<Map<String, String>>> createChildData(List<String[]> childs, String key) {
+        //list for return
+        List<List<Map<String, String>>> listChilds = new LinkedList<>();
+        //List of items
+        List<Map<String,String>> listOneChild;
+        //item
+        Map<String, String> item;
 
-        for(String s : list) {
-            map.put(key, s);
+        //fill this list by data from childs
+        for(String[] strings : childs) {
+            //create List of items
+            listOneChild = new LinkedList<>();
+
+            //fill items
+            for(String s : strings) {
+                item = new HashMap<>();
+                item.put(key,s);
+                listOneChild.add(item);
+            }
+
+            //add list items to base list
+            listChilds.add(listOneChild);
         }
 
-        return map;
+
+        return   listChilds;
+    }
+
+    //Method for getting map of Strings with key
+    private List<Map<String,String>> createGroupData(List<String> list, String key) {
+        List<Map<String,String>> listGroups = new LinkedList<>();
+        //new map
+        Map<String,String> map = new HashMap<>();
+        //fill in the data
+        for(String s : list) {
+            map.put(key, s);
+            listGroups.add(map);
+        }
+        //return map with data
+        return listGroups;
     }
 
 

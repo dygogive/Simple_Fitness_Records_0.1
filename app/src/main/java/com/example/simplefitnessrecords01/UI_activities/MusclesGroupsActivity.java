6cbfc9921b23077f8faff5df6a202bec5114fcce
Researcher <dygogive@gmail.com>
2023-04-06 10:00:46 +0300

    05.04.2023 - ListView muscles

--- app/src/main/java/com/example/simplefitnessrecords01/UI_activities/MusclesGroupsActivity.java
+++ app/src/main/java/com/example/simplefitnessrecords01/UI_activities/MusclesGroupsActivity.java
@@ -1,10 +1,16 @@
 package com.example.simplefitnessrecords01.UI_activities;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.ExpandableListView;
@@ -15,7 +21,6 @@ import android.widget.Toast;
 import com.example.simplefitnessrecords01.R;
 import com.example.simplefitnessrecords01.databinding.ActivityMusclesGroupsBinding;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -23,7 +28,24 @@ import java.util.Map;
 
 public class MusclesGroupsActivity extends AppCompatActivity {
 
+    private static final String GROUP_KEY = "keyGroups";
+    private static final String CHILD_KEY = "keyChilds";
+
+    SimpleExpandableListAdapter simpleExpandableListAdapter = null;
     private ActivityMusclesGroupsBinding binding;
+
+    //Triggers other activities from this activity
+    private ActivityResultLauncher<Intent> activityResultLauncher;
+
+
+
+
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +60,16 @@ public class MusclesGroupsActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
 
 
+        //launcher for activity to chose group of muscles
+        activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if(result.getResultCode() == RESULT_OK) {
+                        Toast.makeText(this, "RESULT_OK", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
         //check if there is ExpandableListview in the activity, check if there is Listview in the activity
         if( binding.lvGroups instanceof ExpandableListView) {
             initExpandableListView();
@@ -47,6 +79,13 @@ public class MusclesGroupsActivity extends AppCompatActivity {
     }
 
 
+
+
+
+
+
+
+    /********************************** LIST VIEWS **********************************/
     private void initListView() {
         //Create ArrayAdapter for ListView and add it to ListView
         ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this,R.array.muscles_groups,android.R.layout.simple_list_item_1);
@@ -67,7 +106,8 @@ public class MusclesGroupsActivity extends AppCompatActivity {
 
 
         //List og groups from resource
-        List<String> groups = Arrays.asList(getResources().getStringArray(R.array.muscles_groups));
+        //Array from resource
+        String[] groups = getResources().getStringArray(R.array.muscles_groups);
         //List og groups with key
         List<Map<String,String>> groupsKey = createGroupData(groups, "keyGroups");
 
@@ -80,29 +120,24 @@ public class MusclesGroupsActivity extends AppCompatActivity {
         childs.add(getResources().getStringArray(R.array.arms));
         childs.add(getResources().getStringArray(R.array.abdominals));
         //list with key
-        List<List<Map<String, String>>> childsKey = createChildData(childs , "keyChilds");
+        List<List<Map<String, String>>> childsKey = createChildData(childs , CHILD_KEY);
 
         //
-        String[] groupFrom = new String[] {"keyGroups"  };
+        String[] groupFrom = new String[] {GROUP_KEY};
         int[] groupTo      = new int[] {R.id.tv_groups};
         //
-        String[] childFrom = new String[] {"keyChilds"  };
+        String[] childFrom = new String[] {CHILD_KEY};
         int[] childTo      = new int[] {R.id.tv_muscles};
 
         ExpandableListView expandableListView = (ExpandableListView) binding.lvGroups;
-        SimpleExpandableListAdapter adapter = new SimpleExpandableListAdapter(
+        simpleExpandableListAdapter = new SimpleExpandableListAdapter(
                 (Context) MusclesGroupsActivity.this,
                 groupsKey, R.layout.text_list_groups_body, groupFrom, groupTo,
                 childsKey, R.layout.text_list_groups_muscles , childFrom, childTo
         );
-        expandableListView.setAdapter(adapter);
+        expandableListView.setAdapter(simpleExpandableListAdapter);
 
-
-
-
-
-
-
+        listenersExpandableListView(expandableListView);
 
     }
 
@@ -135,13 +170,15 @@ public class MusclesGroupsActivity extends AppCompatActivity {
         return   listChilds;
     }
 
+
     //Method for getting map of Strings with key
-    private List<Map<String,String>> createGroupData(List<String> list, String key) {
+    private List<Map<String,String>> createGroupData(String[] array, String key) {
         List<Map<String,String>> listGroups = new LinkedList<>();
         //new map
-        Map<String,String> map = new HashMap<>();
+        Map<String,String> map;
         //fill in the data
-        for(String s : list) {
+        for(String s : array) {
+            map = new HashMap<>();
             map.put(key, s);
             listGroups.add(map);
         }
@@ -151,4 +188,54 @@ public class MusclesGroupsActivity extends AppCompatActivity {
 
 
 
+
+
+
+
+
+
+    /****************************** LISTENERS ***********************************/
+    private void listenersExpandableListView(ExpandableListView expandableListView) {
+        expandableListView.setOnChildClickListener((parent, v, groupPosition, childPosition, id) -> {
+
+            //get chosen names
+            String groupName = getGroupText(expandableListView , groupPosition);
+            String childName = getChildText(expandableListView , groupPosition, childPosition);
+
+            //Toast.makeText(this, groupName + " -> " + childName , Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MusclesGroupsActivity.this, ExercisesList.class);
+            intent.putExtra("muscleGroup" , new String[]{groupName,childName});
+            activityResultLauncher.launch(intent);
+
+
+            return true;
+        });
+    }
+
+    private String getGroupText( ExpandableListView expandableListView , int groupIndex) {
+        Map<String,String> map = (Map<String, String>) simpleExpandableListAdapter.getGroup(groupIndex);
+        return map.get(GROUP_KEY);
+    }
+    private String getChildText(ExpandableListView expandableListView ,int groupIndex, int childIndex) {
+        return ((Map<String,String>) simpleExpandableListAdapter.getChild(groupIndex,childIndex)).get(CHILD_KEY);
+    }
+
+
+
+
+    /************************ OPTIONS MENU *****************************/
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case android.R.id.home:
+                Intent returnIntent = new Intent();
+                returnIntent.putExtra("result musclesGroups", true);
+                setResult(Activity.RESULT_OK, returnIntent);
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
